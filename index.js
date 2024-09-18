7135bffdeed9bc8cf8662de3b61d89bff6d6828e
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2024-09-18 16:39:17 +0200

    Fixes operate function
    One function for each operation was not working,
    the functions could not be called out of the scope.

--- index.js
+++ index.js
@@ -1,40 +1,22 @@
-// operators variables
-//const add;
-//const substract;
-//const multiply;
-//const divide;
-
-// variables
-let firstNum;
-let secondNum;
-let operator;
-
-
 // get values
+let operator = prompt("Type an operator");
 let firstNumber = parseInt(prompt("Type a number"));
 let secondNumber = parseInt(prompt("Type another number"));
 
-// operators functions
-function add(firstNumber, secondNumber) {
-    const add = firstNumber + secondNumber;
-    return console.log(add)
-}
-function substract(firstNumber, secondNumber) {
-    const substract = firstNumber - secondNumber;
-    return console.log(substract);
-}
-function multiply(firstNumber, secondNumber) {
-    const multiply = firstNumber * secondNumber;
-    return console.log(multiply);
-}
-function divide(firstNumber, secondNumber) {
-    const divide = firstNumber / secondNumber;
-    return console.log(divide);
-}
 
-// calling the functions
-add(firstNumber, secondNumber);
-substract(firstNumber, secondNumber);
-multiply(firstNumber, secondNumber);
-divide(firstNumber, secondNumber);
-
+function operate(oper, first, second) {
+    if (operator === "+") {
+        let add = firstNumber + secondNumber;
+        return console.log(add);
+    } else if (operator === "-") {
+        let substract = firstNumber - secondNumber;
+        return console.log(substract);
+    } else if (operator === "*") {
+        let multiply = firstNumber * secondNumber;
+        return console.log(multiply);
+    } else {
+        let divide = firstNumber / secondNumber;
+        return console.log(divide);
+    }
+}
+operate(firstNumber, secondNumber);
