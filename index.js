c5c9a2e7eec0fecf9938b0c4a227def9963e0762
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2024-09-18 13:48:32 +0200

    Step 1 - create operators functions

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+// operators variables
 const add;
 const substract;
 const multiply;
 const divide;
+
+// operators functions
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+function substract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+function divide(firstNumber / secondNumber) {
+    return firstNumber / secondNumber;
+}
