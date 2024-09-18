76dc735062a5897e8c4fae9e591b5fd230e52d17
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2024-09-18 15:58:04 +0200

    Step 2 - create three variables.
    The variables are for each of the parts of a
    calculator operation.

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 // operators variables
-const add;
-const substract;
-const multiply;
-const divide;
+//const add;
+//const substract;
+//const multiply;
+//const divide;
+
+// variables
+let firstNum;
+let secondNum;
+let operator;
+
+
+// get values
+let firstNumber = parseInt(prompt("Type a number"));
+let secondNumber = parseInt(prompt("Type another number"));
 
 // operators functions
 function add(firstNumber, secondNumber) {
-    return firstNumber + secondNumber;
+    const add = firstNumber + secondNumber;
+    return console.log(add)
 }
 function substract(firstNumber, secondNumber) {
-    return firstNumber - secondNumber;
+    const substract = firstNumber - secondNumber;
+    return console.log(substract);
 }
 function multiply(firstNumber, secondNumber) {
-    return firstNumber * secondNumber;
+    const multiply = firstNumber * secondNumber;
+    return console.log(multiply);
 }
-function divide(firstNumber / secondNumber) {
-    return firstNumber / secondNumber;
+function divide(firstNumber, secondNumber) {
+    const divide = firstNumber / secondNumber;
+    return console.log(divide);
 }
+
+// calling the functions
+add(firstNumber, secondNumber);
+substract(firstNumber, secondNumber);
+multiply(firstNumber, secondNumber);
+divide(firstNumber, secondNumber);
+
