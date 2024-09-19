dd5b827e0583dec5761978d94686acfa8e3b9252
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2024-09-19 20:05:53 +0200

    Step 6 - make the calculator work!

--- index.js
+++ index.js
@@ -1,31 +1,68 @@
-// get values
-let firstNumber = parseInt(prompt("Type a number"));
-let operator = prompt("Type an operator");
-let secondNumber = parseInt(prompt("Type another number"));
+// variables
+let n1 = 0
+let op = "+"
+let n2 = ""
 
-// operate function
-function operate(oper, first, second) {
-    if (operator === "+") {
-        let add = firstNumber + secondNumber;
-        return console.log(add);
-    } else if (operator === "-") {
-        let substract = firstNumber - secondNumber;
-        return console.log(substract);
-    } else if (operator === "*") {
-        let multiply = firstNumber * secondNumber;
-        return console.log(multiply);
-    } else {
-        let divide = firstNumber / secondNumber;
-        return console.log(divide);
+// conditional buttons function 
+function pressCalculatorButton(input) {
+    if (input === "0" 
+        || input === "1" 
+        || input === "2" 
+        || input === "3"
+        || input === "4"
+        || input === "5"
+        || input === "6"
+        || input === "7"
+        || input === "8"
+        || input === "9") {
+        n2 = n2 + input;
+    } else if (input === "+"
+        || input === "-"
+        || input === "*"
+        || input === "/"
+    ) {
+        n1 = operate(op, Number(n1), Number(n2));
+        n2 = ""
+        op = input;
+    } else if (input === "=") {
+        n2 = operate(op, Number(n1), Number(n2));
+        n1 = 0;
+        op = "+";
+    } else if (input === "c") {
+        n1 = 0;
+        op = "+";
+        n2 = "";
     }
 }
-operate(firstNumber, secondNumber);
 
-// display the numbers
+// operate function
+function operate(operator, firstNumber, secondNumber) {
+    if (operator === "+") {
+        let add = firstNumber + secondNumber;
+        return add;
+    } else if (operator === "-") {
+        let substract = firstNumber - secondNumber;
+        return substract;
+    } else if (operator === "*") {
+        let multiply = firstNumber * secondNumber;
+        return multiply;
+    } else {
+        let divide = firstNumber / secondNumber;
+        return divide;
+    }
+}
+
+//display the numbers
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        let display = alert(button.id);
+        pressCalculatorButton(button.dataset.value);
+        let display = document.querySelector("#display");
+        if (button.classList.contains("operator")) {
+            display.textContent = n1;
+        } else {
+        display.textContent = Number(n2);
+        }
     })
 }
 )
